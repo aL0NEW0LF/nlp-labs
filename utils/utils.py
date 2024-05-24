685a83e4267c49be2a2928ccf58110fc7dd2c440
aL0NEW0LF <mohamedfakhreeddine2019@gmail.com>
2024-05-24 15:35:24 +0100

    feat: part 1

--- utils/utils.py
+++ utils/utils.py
@@ -100,6 +100,19 @@ def vectorize_answer(answer_tokens, word2vec_model):
     answer_vector = pd.Series([np.mean([word2vec_model.wv[word] for word in sentence if word in word2vec_model.wv] or [np.zeros(word2vec_model.vector_size)], axis=0) for sentence in answer_tokens])
     return answer_vector
 
+def vectorize_dataframe(dataframe, sg=0, vector_size=100):
+    """
+    Vectorizes a dataframe using Word2Vec.
+    Args:
+        - dataframe (pd.DataFrame): Dataframe to vectorize.
+        - sg (int): Skip-gram parameter for Word2Vec.
+        - vector_size (int): Vector size for Word2Vec.
+    """
+    cbow_model = Word2Vec(dataframe['content'], vector_size=vector_size, window=5, sg=sg, min_count=1)
+    text_vectors = vectorize_answer(dataframe['content'], cbow_model)
+    return pd.concat([pd.DataFrame(text_vectors.values.tolist(), index=text_vectors.index), dataframe['score']], axis=1)
+
+
 def finetune_mse_r2(dataframe, model, sg=0, test_size=0.1, min_vector_size=1, max_vector_size=1000):
     """ 
     Finetunes a regression model using Word2Vec embeddings and returns the best MSE, R2 and vector size.
@@ -237,3 +250,62 @@ def evaluate_clf_model(dataframe_train, dataframe_val, model, vector_size, sg=0)
     print('Accuracy: ', acc, ' - F1: ', f1)
 
     return acc, f1
+
+def encode_sentence(text, vocab2index, N=200):
+    tokenized = tokenize(text)
+    encoded = np.zeros(N, dtype=int)
+    enc1 = np.array([vocab2index.get(word, vocab2index["UNK"]) for word in tokenized])
+    length = min(N, len(enc1))
+    encoded[:length] = enc1[:length]
+    return encoded, length
+
+# We can load the vectors using our custom functions
+def load_glove_vectors(glove_file = '../utils/models/vectors.txt'):
+    """Load the glove word vectors"""
+    word_vectors = {}
+    with open(glove_file, encoding='utf-8') as f:
+        for line in f:
+            split = line.split()
+            word_vectors[split[0]] = np.array([float(x) for x in split[1:]])
+    return word_vectors
+
+def get_emb_matrix(pretrained, word_counts, emb_size = 256):
+    """ Creates embedding matrix from word vectors"""
+    vocab_size = len(word_counts) + 2
+    vocab_to_idx = {}
+    vocab = ["", "UNK"]
+    W = np.zeros((vocab_size, emb_size), dtype="float32")
+    W[0] = np.zeros(emb_size, dtype='float32') # adding a vector for padding
+    W[1] = np.random.uniform(-0.25, 0.25, emb_size) # adding a vector for unknown words 
+    vocab_to_idx["UNK"] = 1
+    i = 2
+    for word in word_counts:
+        if word in pretrained:
+            W[i] = pretrained[word]
+        else:
+            W[i] = np.random.uniform(-0.25,0.25, emb_size)
+        vocab_to_idx[word] = i
+        vocab.append(word)
+        i += 1   
+    return W, np.array(vocab), vocab_to_idx
+
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
